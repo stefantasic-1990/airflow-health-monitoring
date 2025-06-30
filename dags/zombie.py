71be7b4ee58b6de6a193299ba52de173768505bf
Stefan Tasic <stefantasic.4293@gmail.com>
2025-06-30 16:10:32 -0400

    rename zombie dag

--- dags/zombie.py
+++ dags/zombie.py
@@ -5,19 +5,19 @@ import signal
 import time
 
 @dag(
-    dag_id="zombie",
+    dag_id="zombie_sim",
     schedule=None,
     start_date = timezone.datetime(2025, 1, 1),
     catchup=False,
     tags=["failure_simulation"],
 )
-def zombie():
+def zombie_sim():
 
     @task
     def terminate_me():
-        print("Task started and sleeping for 1 minute. Kill the worker container now to simulate a zombie.")
+        print("Task started and sleeping for 1 minute. Kill the worker container now to simulate a zombie task.")
         time.sleep(60)
 
     terminate_me()
 
-dag = zombie()
+dag = zombie_sim()
