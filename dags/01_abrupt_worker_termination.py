c4c976454737661cac9dfcee243c15f2213ac1aa
Stefan Tasic <stefan.tasic@points.com>
2025-07-14 06:39:40 -0400

    add comfortable sleeper to first scenario dag

--- dags/01_abrupt_worker_termination.py
+++ dags/01_abrupt_worker_termination.py
@@ -12,10 +12,15 @@ import time
 def abrupt_worker_termination():
 
     @task
-    def terminate_me():
-        print("Task started and sleeping for 60 seconds. Kill the worker container now to simulate a zombie task.")
-        time.sleep(60)
+    def interrupted_sleeper():
+        print("Task started and sleeping for 20 seconds. Kill the worker container now to simulate a zombie task.")
+        time.sleep(20)
 
-    terminate_me()
+    def comfortable_sleeper():
+        print("My purpose in this DAG is to sleep without interruption.")
+        time.sleep(20)
+
+    interrupted_sleeper()
+    comfortable_sleeper()
 
 dag = abrupt_worker_termination()
